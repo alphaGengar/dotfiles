584eef184c7bd5b5ee6c851c03373854edb4b002
Swaraj Adhikari <swaraj2065@gmail.com>
2026-02-18 14:06:36 -0800

    some changes

--- .config/sketchybar/plugins/spotify.sh
+++ .config/sketchybar/plugins/spotify.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # Max number of characters so it fits nicely to the right of the notch
 # MAY NOT WORK WITH NON-ENGLISH CHARACTERS
-MAX_LENGTH=33
+MAX_LENGTH=28
 HALF_LENGTH=$(((MAX_LENGTH + 1) / 2))
 SPOTIFY_JSON="$INFO"
 COVER_PATH="/tmp/cover.jpg"
 PREVIOUS_COVER=""
+
+TITLE_ITEM="spotify.title"
+PLAY_ITEM="spotify.play"
+PLAY_ICON=""
 
 # Function to update album cover art
 update_cover_art() {
@@ -16,57 +20,84 @@ update_cover_art() {
             curl -s --max-time 200 "$COVER" -o "$COVER_PATH.tmp" && \
             if [ -f "$COVER_PATH.tmp" ]; then
                 mv "$COVER_PATH.tmp" "$COVER_PATH"
-                sketchybar --set cover background.image="$COVER_PATH" \
-                                    background.image.scale=0.040 \
-                                    background.image.corner_radius=7 \
-                                    background.image.padding_left=18
+                sketchybar --set cover \
+                    background.image="$COVER_PATH" \
+                    background.image.scale=0.040 \
+                    background.image.corner_radius=7 \
+                    background.image.padding_left=18
             fi
         ) &
     fi
 }
-# Function to update track information
-update_track() {
-    if [[ -z $SPOTIFY_JSON ]]; then
-        sketchybar --set $NAME icon.color=0xffeed49f label.drawing=no
-        return
-    fi
-    PLAYER_STATE=$(echo "$SPOTIFY_JSON" | jq -r '.["Player State"]')
-    if [ $PLAYER_STATE = "Playing" ]; then
-        TRACK="$(echo "$SPOTIFY_JSON" | jq -r .Name)"
-        ARTIST="$(echo "$SPOTIFY_JSON" | jq -r .Artist)"
-        # Calculations so it fits nicely
-        TRACK_LENGTH=${#TRACK}
-        ARTIST_LENGTH=${#ARTIST}
-        if [ $((TRACK_LENGTH + ARTIST_LENGTH)) -gt $MAX_LENGTH ]; then
-            # If the total length exceeds the max
-            if [ $TRACK_LENGTH -gt $HALF_LENGTH ] && [ $ARTIST_LENGTH -gt $HALF_LENGTH ]; then
-                # If both the track and artist are too long, cut both at half length - 1
-                TRACK="${TRACK:0:$((MAX_LENGTH % 2 == 0 ? HALF_LENGTH - 2 : HALF_LENGTH - 1))}…"
-                ARTIST="${ARTIST:0:$((HALF_LENGTH - 2))}…"
-            elif [ $TRACK_LENGTH -gt $HALF_LENGTH ]; then
-                # Else if only the track is too long, cut it by the difference of the max length and artist length
-                TRACK="${TRACK:0:$((MAX_LENGTH - ARTIST_LENGTH - 1))}…"
-            elif [ $ARTIST_LENGTH -gt $HALF_LENGTH ]; then
-                ARTIST="${ARTIST:0:$((MAX_LENGTH - TRACK_LENGTH - 1))}…"
-            fi
+
+format_label() {
+    local track="$1"
+    local artist="$2"
+
+    local track_length=${#track}
+    local artist_length=${#artist}
+
+    if [ $((track_length + artist_length)) -gt $MAX_LENGTH ]; then
+        if [ $track_length -gt $HALF_LENGTH ] && [ $artist_length -gt $HALF_LENGTH ]; then
+            track="${track:0:$((MAX_LENGTH % 2 == 0 ? HALF_LENGTH - 2 : HALF_LENGTH - 1))}…"
+            artist="${artist:0:$((HALF_LENGTH - 2))}…"
+        elif [ $track_length -gt $HALF_LENGTH ]; then
+            track="${track:0:$((MAX_LENGTH - artist_length - 1))}…"
+        elif [ $artist_length -gt $HALF_LENGTH ]; then
+            artist="${artist:0:$((MAX_LENGTH - track_length - 1))}…"
         fi
-        
-        sketchybar --set $NAME label="${TRACK}   ${ARTIST}" label.drawing=yes icon.color=0xffa6da95
-        update_cover_art # Fetch and update the cover art
-    elif [ $PLAYER_STATE = "Paused" ]; then
-        sketchybar --set $NAME icon.color=0xffeed49f
-    elif [ $PLAYER_STATE = "Stopped" ]; then
-        sketchybar --set $NAME icon.color=0xffeed49f label.drawing=no
+    fi
+
+    if [ -n "$artist" ]; then
+        printf '%s  %s' "$track" "$artist"
     else
-        sketchybar --set $NAME icon.color=0xffeed49f
+        printf '%s' "$track"
     fi
 }
 
-case "$SENDER" in
-"mouse.clicked")
-    osascript -e 'tell application "Spotify" to playpause'
-    ;;
-*)
-    update_track
-    ;;
-esac
+update_state() {
+    if [[ -z $SPOTIFY_JSON ]]; then
+        sketchybar --set "$TITLE_ITEM" label.drawing=off icon.color=0xffeed49f
+        sketchybar --set "$PLAY_ITEM" icon="$PLAY_ICON" icon.color=0xffeed49f
+        return
+    fi
+
+    PLAYER_STATE=$(echo "$SPOTIFY_JSON" | jq -r '."Player State"')
+    TRACK=$(echo "$SPOTIFY_JSON" | jq -r .Name)
+    ARTIST=$(echo "$SPOTIFY_JSON" | jq -r .Artist)
+
+    LABEL=""
+    if [ -n "$TRACK" ] || [ -n "$ARTIST" ]; then
+        LABEL=$(format_label "$TRACK" "$ARTIST")
+    fi
+
+    case "$PLAYER_STATE" in
+    "Playing")
+        ICON_COLOR=0xffa6da95
+        LABEL_DRAWING=on
+        update_cover_art
+        ;;
+    "Paused")
+        ICON_COLOR=0xffeed49f
+        LABEL_DRAWING=on
+        ;;
+    "Stopped")
+        ICON_COLOR=0xffeed49f
+        LABEL_DRAWING=off
+        ;;
+    *)
+        ICON_COLOR=0xffeed49f
+        LABEL_DRAWING=off
+        ;;
+    esac
+
+    if [ -n "$LABEL" ] && [ "$LABEL_DRAWING" = "on" ]; then
+        sketchybar --set "$TITLE_ITEM" label="$LABEL" label.drawing=on icon.color=$ICON_COLOR
+    else
+        sketchybar --set "$TITLE_ITEM" label.drawing=off icon.color=$ICON_COLOR
+    fi
+
+    sketchybar --set "$PLAY_ITEM" icon="$PLAY_ICON" icon.color=$ICON_COLOR
+}
+
+update_state
