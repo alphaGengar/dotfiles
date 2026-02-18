ff66bb6550e16b6daa0dff3354efe09f07009cdc
Swaraj Adhikari <swaraj2065@gmail.com>
2026-02-18 13:04:23 -0800

    some changes

--- .config/sketchybar/lib/common.sh
+++ .config/sketchybar/lib/common.sh
@@ -3,5 +3,5 @@
 SKETCHYBAR_CONFIG="${SKETCHYBAR_CONFIG:-$HOME/.config/sketchybar}"
 
 FONT_FACE="JetBrainsMono Nerd Font"
-PLUGIN_SHARED_DIR="$SKETCHYBAR_CONFIG/plugins"
+PLUGIN_DIR="$SKETCHYBAR_CONFIG/plugins"
 SPOTIFY_EVENT="com.spotify.client.PlaybackStateChanged"
